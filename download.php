c398b5ab9e99105c10ac88aa0fc46695cfcc6d5f
bbeonis-cmd <bbeonis@gmail.com>
2026-07-10 01:57:08 +0900

    Update download.php

--- download.php
+++ download.php
@@ -6,8 +6,7 @@ $res = $conn->query("SELECT * FROM files WHERE id = $file_id");
 if ($res && $res->num_rows > 0) {
     $file = $res->fetch_assoc();
     $file_path = __DIR__ . '/uploads/' . $file['save_name'];
-    echo "컴퓨터가 찾고 있는 실제 경로: " . $file_path; exit;
-
+    
     if (file_exists($file_path)) {
         header('Content-Description: File Transfer');
         header('Content-Type: application/octet-stream');
